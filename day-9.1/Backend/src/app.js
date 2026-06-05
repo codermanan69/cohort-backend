ddd21437c147c710f7b5dc4bdb060508f70913d6
codermanan69 <manansahni295@gmail.com>
2026-06-06 02:21:25 +0530

    fix: change catch-all route from app.use to app.get and fix path

--- day-9.1/Backend/src/app.js
+++ day-9.1/Backend/src/app.js
@@ -55,7 +55,8 @@ app.patch('/api/notes/:id', async (req , res ) => {
     })
 })
 
-app.use ('*', (req , res) => {
-res.sendFile(path.join(__dirname,"..", "/public/index.html"))
+app.get('*', (req , res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"))
 })
+
 module.exports = app
